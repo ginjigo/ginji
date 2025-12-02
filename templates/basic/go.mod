49393973bf885a9db4a015608821e4bdceaaf0c5
kalanakt <e19198@eng.pdn.ac.lk>
2025-12-02 23:42:27 +0530

    Feat: Make basic template dynamic (ORM/DB conditionals)

--- templates/basic/go.mod
+++ templates/basic/go.mod
@@ -4,6 +4,10 @@ go 1.23.0
 
 require (
 	github.com/ginjigo/ginji v0.1.9
-	gorm.io/driver/sqlite v1.5.6
+{{if eq .ORM "GORM"}}
 	gorm.io/gorm v1.25.12
+{{if eq .Database "SQLite"}}	gorm.io/driver/sqlite v1.5.6{{end}}
+{{if eq .Database "PostgreSQL"}}	gorm.io/driver/postgres v1.5.9{{end}}
+{{if eq .Database "MySQL"}}	gorm.io/driver/mysql v1.5.7{{end}}
+{{end}}
 )
--- templates/basic/internal/database/database.go
+++ templates/basic/internal/database/database.go
@@ -1,16 +1,29 @@
 package database
 
+{{if eq .ORM "GORM"}}
 import (
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+{{if eq .Database "SQLite"}}	"gorm.io/driver/sqlite"{{end}}
+{{if eq .Database "PostgreSQL"}}	"gorm.io/driver/postgres"{{end}}
+{{if eq .Database "MySQL"}}	"gorm.io/driver/mysql"{{end}}
 )
 
 var DB *gorm.DB
 
 func Connect() {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{})
+{{if eq .Database "SQLite"}}	DB, err = gorm.Open(sqlite.Open("app.db"), &gorm.Config{}){{end}}
+{{if eq .Database "PostgreSQL"}}	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable"
+	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{}){{end}}
+{{if eq .Database "MySQL"}}	// dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	// DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{}){{end}}
+
 	if err != nil {
 		panic("failed to connect database")
 	}
 }
+{{else}}
+func Connect() {
+	// TODO: Implement database connection
+}
+{{end}}
